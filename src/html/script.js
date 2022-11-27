3d422ff4fd4583f0bd0a62a8b461a69dd8499de3
Mark Biesheuvel <mail@markbiesheuvel.nl>
2022-11-27 17:21:43 +0100

    Add labels to indicate length of each edge

--- src/html/script.js
+++ src/html/script.js
@@ -1,5 +1,5 @@
 class Design {
-  constructor(button) {
+  constructor(button, svgDoc) {
     this.button = button;
     this.id = button.dataset.id;
     this.group = svgDoc.querySelector(`g[data-id="design-${this.id}"]`);
@@ -81,23 +81,23 @@ class Floor {
 
 // Wait for the SVG to be loaded before enabling interaction
 const svg = document.getElementById('svg');
-let svgDoc;
 let activeDesign = null;
 let activeFloorButton = null;
 svg.addEventListener('load', () => {
 
   // Get inner document from SVG
-  svgDoc = svg.contentDocument;
+  const svgDoc = svg.contentDocument;
 
-  // Iterate over all buttons
+  // Iterate over all buttons that target design
   const buttons = document.querySelectorAll('button[data-target="design"]');
   const designs = Array.from(buttons).map((button) => {
-    return new Design(button);
+    return new Design(button, svgDoc);
   });
 
   // Activate first design
   designs[0].activate();
 
+  // Iterate over all buttons that target floor
   document.querySelectorAll('button[data-target="floor"]').forEach((button, index) => {
     // First one is already active
     if (index == 0) {
@@ -119,5 +119,74 @@ svg.addEventListener('load', () => {
         activeFloorButton = button;
       }
     })
+  });
+
+  // Iterate over all paths inside designs
+  const createText = (length, x, y, rotate=false) => {
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    text.classList.add('small');
+    text.textContent = `${Math.abs(length/100).toFixed(2)}m`;
+    text.setAttribute('filter', 'url(#solid-bg)')
+    if (rotate) {
+      text.setAttribute('transform', `translate(${x},${y}) rotate(90)`);
+    } else {
+      text.setAttribute('transform', `translate(${x},${y})`);
+    }
+    return text;
+  }
+  svgDoc.querySelectorAll('g[data-role="design"] path').forEach((path) => {
+    const parent = path.parentNode;
+    const d = path.getAttribute('d');
+    const segments = d.split(/(?=[vhZ])/);
+    let originX = x = 0;
+    let originY = y = 0;
+    let matches;
+    segments.forEach((segment) => {
+      // M for move
+      matches = segment.match(/M\s+(\d+)\s+(\d+)/);
+      if (matches) {
+        originX = x = parseInt(matches[1]);
+        originY = y = parseInt(matches[2]);
+      }
+
+      // h for horizontal line
+      matches = segment.match(/h\s+(-?\d+)/);
+      if (matches) {
+        const width = parseInt(matches[1]);
+
+        // Add text element in the middle of the line
+        const text = createText(width, x + width / 2, y, false);
+        parent.appendChild(text);
+
+        // Update coordinates
+        x += width;
+      }
+
+      // v for vertical line
+      matches = segment.match(/v\s+(-?\d+)/);
+      if (matches) {
+        const height = parseInt(matches[1]);
+
+        // Add text element in the middle of the line
+        const text = createText(height, x, y + height / 2, true);
+        parent.appendChild(text);
+
+        // Update coordinates
+        y += height;
+      }
+
+      // Z is back to home
+      if (segment.match(/Z/)) {
+        const dx = originX - x;
+        const dy = originY - y;
+        if (Math.abs(dy) < Math.abs(dx)) { // Horizontal line
+          const text = createText(dx, x + dx / 2, y, false);
+          parent.appendChild(text);
+        } else { // Vertical line
+          const text = createText(dy, x, y + dy / 2, true);
+          parent.appendChild(text);
+        }
+      }
+    });
   });
 });
